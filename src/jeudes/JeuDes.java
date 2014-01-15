61791a1bc31620e1cc9e38010771c05e6b226307
Nicholas Vezina <compagniesnichalex@gmail.com>
2014-01-15 18:29:18 -0500

    Sources de l'Épisode 8 + correctif
    La correction est de réinistialisé le contenu de tabNombreDes à zéro.

--- src/jeudes/JeuDes.java
+++ src/jeudes/JeuDes.java
@@ -27,6 +27,7 @@ public class JeuDes
         String joueur2 = "Joueur 2";
         int [] tabDes = new int[5];
         Random random = new Random();
+        int [] tabNombreDes = new int[6];
         boolean [] tabBoolean = new boolean[tabDes.length];
         
         while(choix != 4)
@@ -43,19 +44,144 @@ public class JeuDes
                     joueur2 = changerNomJoueur();
                     break;
                 case 3 :
-                    jouerPartie(joueur1,joueur2,tabDes,tabBoolean,random);
+                    jouerPartie(joueur1,joueur2,tabDes,tabBoolean,random,tabNombreDes);
             }
         }
     }
-    private static void jouerPartie(String joueur1,String joueur2,int[]tabDes,boolean[]tabBoolean,Random random) throws IOException
+    private static void jouerPartie(String joueur1,String joueur2,int[]tabDes,boolean[]tabBoolean,Random random,int [] tabNombreDes) throws IOException
     {
+        int total = 0;
         genererDes(tabDes,random);
-        afficherDes(tabDes);
+        reinitialiserTabNombreDes(tabNombreDes);
+        calculerNombreDes(tabNombreDes,tabDes);
+        total = calculerPoints(tabNombreDes);
+        afficherDes(tabDes,total);
         String choix = lireChoixPartie();
         //135
         verifierChoix(tabBoolean,choix);
+        if(verifierRebrassage(tabBoolean))
+        {
         rebrasserDes(tabBoolean,tabDes,random);
-        afficherDes(tabDes);
+        reinitialiserTabNombreDes(tabNombreDes);
+        calculerNombreDes(tabNombreDes,tabDes);
+        total = calculerPoints(tabNombreDes);
+        afficherDes(tabDes,total);
+        }
+    }
+    
+    private static void reinitialiserTabNombreDes(int[] tabNombreDes)
+    {
+        //Réinitialise notre tableau de dés à 0.
+        for(int i = 0; i < tabNombreDes.length; i++)
+        {
+            tabNombreDes[i] = 0;
+        }
+    }
+    
+    private static int calculerPoints(int[] tabNombreDes)
+    {
+        int total = 0;
+        int totalBrelan = 0;
+        int totalFull = 0;
+        int totalSuite = 0;
+        int totalAutres = 0;
+        
+        totalBrelan = calculerBrelan(tabNombreDes);
+        totalFull = calculerFull(totalBrelan,tabNombreDes);
+        totalSuite = calculerSuite(tabNombreDes);
+        totalAutres = calculerAutres(totalFull,totalSuite,tabNombreDes);
+        
+        total += totalBrelan + totalFull + totalSuite + totalAutres;
+        return total;
+    }
+    
+    private static int calculerAutres(int totalFull, int totalSuite, int[] tabNombreDes)
+    {
+        int totalAutres = 0;
+        
+        if( totalFull == 0 && totalSuite == 0)
+        {
+            if(tabNombreDes[0] <= 2 && tabNombreDes[0] > 0)
+            {
+                totalAutres += 100 * tabNombreDes[0];
+            }
+            if ( tabNombreDes[4] <= 2 && tabNombreDes[4] > 0)
+            {
+                totalAutres += 50 * tabNombreDes[4];
+            }
+        }
+        return totalAutres;
+    }
+    
+    private static int calculerSuite(int [] tabNombreDes)
+    {
+        int totalSuite = 0;
+        
+        if(tabNombreDes[0] == 1 && tabNombreDes[1] == 1 
+                && tabNombreDes[2] == 1 && tabNombreDes[3] == 1
+                && tabNombreDes[4] == 1 || tabNombreDes[1] == 1 
+                && tabNombreDes[2] == 1 && tabNombreDes[3] == 1 
+                && tabNombreDes[4] == 1 && tabNombreDes[5] == 1)
+        {
+            totalSuite += 1500;
+        }
+        return totalSuite;
+    }
+    
+    private static int calculerFull(int totalBrelan,int [] tabNombreDes)
+    {
+        int totalFull = 0;
+        
+        if(totalBrelan > 0)
+        {
+          for(int i = 0; i < tabNombreDes.length; i++)
+          {
+              if(tabNombreDes[i] == 2)
+              {
+                  totalFull += (i + 1) * 50;
+              }
+          }
+        }
+        return totalFull;
+    }
+    
+    private static int calculerBrelan(int [] tabNombreDes)
+    {
+        int totalBrelan = 0;
+        for(int i = 0; i < tabNombreDes.length; i++)
+        {
+            if(tabNombreDes[i] >= 3)
+            {
+                totalBrelan += (i + 1) * 100;
+            }
+        }
+        return totalBrelan;
+    }
+    
+    private static boolean verifierRebrassage(boolean[] tabBoolean)
+    {
+        for(int i = 0; i < tabBoolean.length;i++)
+        {
+            if(tabBoolean[i])
+            {
+                return true;
+            }
+        }
+            return false;
+    }
+    
+    private static void calculerNombreDes(int[] tabNombreDes,int [] tabDes)
+    {
+        for(int i = 0; i < tabDes.length; i++)
+        {
+            for(int j = 0; j < tabNombreDes.length; j++)
+            {
+                if(tabDes[i] == j + 1)
+                {
+                    tabNombreDes[j]++;
+                }
+            }
+        }
     }
     
     private static void rebrasserDes(boolean[] tabBoolean,int[] tabDes, Random random)
@@ -91,7 +217,7 @@ public class JeuDes
     
     private static String lireChoixPartie() throws IOException
     {
-        System.out.println("Veuillez choisir le ou les dés à rebrasser : (1 à 5)");
+        System.out.println("Veuillez choisir le ou les dés à rebrasser : (1 à 5) ");
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         return r.readLine();
     }
@@ -104,13 +230,13 @@ public class JeuDes
         }
     }
     
-    private static void afficherDes(int[] tabDes)
+    private static void afficherDes(int[] tabDes,int total)
     {
         for(int i = 0; i < tabDes.length; i++)
         {
             System.out.print(tabDes[i] + " ");
         }
-        System.out.println();
+        System.out.println("Points : " + total);
     }
     
     private static void afficherMenuPrincipal()
